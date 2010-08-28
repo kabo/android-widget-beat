e290c509747aee373a5e02f3ee1ca8df5ae165ac
kabo <kabo@localhost>
2010-08-28 22:57:49 +0200

    Using thread for updates once a beat

--- src/nu/kabo/android/beat/Beat.java
+++ src/nu/kabo/android/beat/Beat.java
@@ -3,83 +3,82 @@ package nu.kabo.android.beat;
 import java.util.Calendar;
 import java.util.TimeZone;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.os.SystemClock;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.widget.RemoteViews;
 
 public class Beat extends AppWidgetProvider {
 	
-	private static final String TAG = "Beat";
-	public static final int UPDATE = 1;
+	protected static final String TAG = "Beat";
+	protected Thread notifyingThread;
+	protected ComponentName thisWidget;
+	protected RemoteViews remoteViews;
+	protected AppWidgetManager appWidgetManager;
+	
+	private Handler handler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			UpdateTime();
+		}
+	};
+	
+	private Runnable mTask = new Runnable() {
+        public void run() {
+        	try {
+        		handler.sendEmptyMessage(0);
+        		double cur_time = GetTime();
+        		double cur_time_dec = cur_time-(int)cur_time;
+				Thread.sleep((int)Math.ceil(87000*(1-cur_time_dec))); // some extra time here
+			} catch (InterruptedException e) {
+				return;
+			} catch (Exception e) {
+				Log.e(TAG, "Error: " + e.getMessage(), e);
+			}
+        	while(true) {
+        		try {
+            		handler.sendEmptyMessage(0);
+            		Thread.sleep(86400); // sleep one beat
+				} catch (InterruptedException e) {
+					Log.d(TAG, "Caught interrupt, exiting thread");
+					return;
+				} catch (Exception e) {
+					Log.e(TAG, "Error: " + e.getMessage(), e);
+				}
+        	}
+        }
+    };
+    
+	public void onEnabled(Context context) {
+		thisWidget = new ComponentName(context, Beat.class);
+		remoteViews = new RemoteViews(context.getPackageName(), R.layout.main);
+		appWidgetManager = AppWidgetManager.getInstance(context);
+		notifyingThread = new Thread(mTask, "NotifyingService");
+		notifyingThread.start();
+	}
 	
 	public void onDisabled(Context context) {
-		//context.unregisterReceiver(this);
-		stopTimer(context);
+		// exit, causing the notifyingThread to die as well
+		System.exit(0);
 	}
 	
-	public void onEnabled(Context context) {
-		// sätt alarm-manager på att trigga onUpdate
-		//context.registerReceiver(this, new IntentFilter("android.intent.action.TIME_TICK"));
-		Log.d(TAG, "enabled");
-		startTimer(context);
+	private void UpdateTime() {
+		Log.d(TAG, "Updating time");
+		remoteViews.setTextViewText(R.id.widget_textview,
+				"@" + GetTimeString());
+		appWidgetManager.updateAppWidget(thisWidget, remoteViews);
 	}
 	
-	protected PendingIntent getPendingIntentUpdate(Context context) {
-		Intent i = new Intent();
-		i.setAction("nu.kabo.android.beat.UPDATE");
-		i.setClassName(context.getPackageName(), Beat.class.getName());
-		return PendingIntent.getBroadcast(
-				context, 0, i, PendingIntent.FLAG_CANCEL_CURRENT
-		);
+	public double GetTime() {
+		Calendar t = Calendar.getInstance(TimeZone.getTimeZone("GMT+0100"));
+		return (double)(( ( ( t.get(Calendar.HOUR_OF_DAY) + (t.get(Calendar.MINUTE)/60.0) + (t.get(Calendar.SECOND)/3600.0) ) ) / 24.0 ) * 1000.0);
 	}
-	
-	protected void startTimer(Context context) {
-		Log.d(TAG, "startTimer");
-		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-		alarmManager.setRepeating(
-				AlarmManager.ELAPSED_REALTIME, 
-				SystemClock.elapsedRealtime()+1000, 
-				10000, 
-				getPendingIntentUpdate(context)
-		);
-	}
-	
-	protected void stopTimer(Context context) {
-		Log.d(TAG, "stopTimer");
-		AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-		alarmManager.cancel(getPendingIntentUpdate(context));
-	}
-	
-	public void onReceive(Context context, Intent intent) {
-		super.onReceive(context, intent);
-		Log.d(TAG, intent.getAction());
-		if (intent.getAction().equals("nu.kabo.android.beat.UPDATE")) {
-			Log.d(TAG, "Update");
-			RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.main);
-			ComponentName thisWidget = new ComponentName(context, Beat.class);
-			AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-			
-			remoteViews.setTextViewText(R.id.widget_textview,
-					"@" + calculateITime(Calendar.getInstance(TimeZone.getTimeZone("GMT+0100"))));
-			appWidgetManager.updateAppWidget(thisWidget, remoteViews);
-		} else if (intent.getAction().equals("android.intent.action.SCREEN_ON")) {
-			Log.d(TAG, "screen_on");
-			startTimer(context);
-		} else if (intent.getAction().equals("android.intent.action.SCREEN_OFF")) {
-			Log.d(TAG, "screen_off");
-			stopTimer(context);
-		}
-	}
-	
-	public String calculateITime(Calendar t) {
-		String ITime = "00" + (int)(( ( ( t.get(Calendar.HOUR_OF_DAY) + (t.get(Calendar.MINUTE)/60.0) + (t.get(Calendar.SECOND)/3600.0) ) ) / 24.0 ) * 1000);
+	public String GetTimeString() {
+		String ITime = "00" + (int)GetTime();
 		return (ITime.substring(ITime.length()-3));
 	}
 	
